a703ad60a78cf31c85cb2e329a67706a0f5027d2
Wojciech Mazur <wmazur@virtuslab.com>
2025-05-15 15:21:23 +0200

    Prepare exmaple/bzlmod to be used in bcr presubmit

---
+++ example/bzlmod/mylib/mylib.h
@@ -0,0 +1,7 @@
+
+#include <string>
+
+namespace mylib {
+int add(int a, int b);
+std::string greet(const std::string &name);
+} 
---
+++ example/bzlmod/mylib/mylib_test.cc
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include <string>
+#include "mylib.h"
+
+TEST(AdditionTest, HandlesVariousInputs) {
+    EXPECT_EQ(mylib::add(1, 2), 3);
+    EXPECT_EQ(mylib::add(-5, 5), 0);
+    EXPECT_EQ(mylib::add(0, 0), 0);
+}
+
+TEST(GreetTest, ProducesCorrectGreeting) {
+    EXPECT_EQ(mylib::greet("Alice"), "Hello, Alice!");
+    EXPECT_EQ(mylib::greet("Bob"), "Hello, Bob!");
+}
